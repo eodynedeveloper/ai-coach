f162cf54372c74a7a6fcf8db13838231a61c816b
Rajsuryan Singh <rajsuryan.singh@gmail.com>
2023-08-24 09:16:47 +0200

    fix path issue in utils

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,10 @@
 import json
 import os
 import sys 
-path = sys.path[0].split("/")
-path = "/".join(path[:-1])
+path = sys.path[0]
 
 def load_msgs(language="English"):
-    fpath = f"msgs_{language}.json"
+    fpath = f"{path}/msgs_{language}.json"
     if not os.path.exists(fpath):
         raise ValueError(f"Invalid Language: {language}")
     with open(fpath, "r") as file:
